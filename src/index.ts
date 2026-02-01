445653d9a1b9b10fcaded9e03eb8f9d684b11628
Suman Dhami <sumandhami86@gmail.com>
2026-02-01 23:01:31 +0545

    configured better auth

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import subjectsRouter from "./routes/subject";
 import cors from 'cors';
 import securityMiddleware from "./middleware/security";
+import {toNodeHandler} from "better-auth/node";
+import {auth} from "./lib/auth";
 
 const app = express();
 const port = 8000;
@@ -13,6 +15,8 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
 app.use(express.json());
 
